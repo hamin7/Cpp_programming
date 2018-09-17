fac417c876739fda7026dd4f668775de1f85e85b
hamin7 <33690039+hamin7@users.noreply.github.com>
2018-09-17 11:22:17 +0900

    Create 18.-9.17.cpp

---
+++ 18.-9.17.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+//#define NUM 45
+const int NUM = 45;
+
+int main() {
+
+	int i = 3;
+	if( i = 4 ) { // equality is ==
+		cout<<"True branch"<<endl;
+	}
+	else
+		cout<<"False branch"<<endl;
+	
+	return 0;
+}
+
+
+//if ( i = 4 ); ??
