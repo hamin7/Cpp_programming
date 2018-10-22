59bec4f837fa67aa9d468f682896c9640feb9a47
hamin7 <33690039+hamin7@users.noreply.github.com>
2018-10-23 03:37:03 +0900

    Update sum_down.cpp

--- Homework/sum_down.cpp
+++ Homework/sum_down.cpp
@@ -1,21 +1,26 @@
 #include <iostrem>
-int sum_down(int x)
-{
-  if (x>=0){
-    return (x-1) + 2 * sum_down(x-1);
+
+int sum_down(int x){
+  if (x >= 0){
+    return (x - 1) + 2*sum_down(x-1);
   }
   else{
     return 1;
   }
 }
+
 int sum(int n){
   int sum = 1;
   for (int j = -1; j < n; j++){
-    sum = j + 2 * sum;
+    sum = j + 2*sum;
   }
   return sum;
 }
-void main(){
-  std::cout << sum_down(2) << std::endl;
-  std::cout << sum(2) << std::endl;
+
+int main(){
+  int x;
+  std::cout << "Enter the integer ";
+  std::cin >> x;
+  std::cout << sum_down(x) << std::endl;
+  std::cout << sum(x) << std::endl;
 }
