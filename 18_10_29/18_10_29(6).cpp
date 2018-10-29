15b261b69febcd291317a837fe972e84c4581cff
hamin7 <33690039+hamin7@users.noreply.github.com>
2018-10-29 11:42:14 +0900

    Update 18_10_29(6).cpp

--- 18_10_29/18_10_29(6).cpp
+++ 18_10_29/18_10_29(6).cpp
@@ -3,7 +3,7 @@
 class Circle{
 public:
   Circle(){     // default Constructor
-    std::cout << "Destructor called ... " << std::endl;
+    std::cout << "Default Constructor called ... " << std::endl;
     radius = 0.0;
   }     // Constructor
   const double radius;     // attribute
@@ -18,6 +18,6 @@ public:
 
 void main()
 {
-  Circle c(3.0);     // initialization of public members only!   // defines an object
+  Circle c;     // initialization of public members only!   // defines an object
   std::cout << c.area() << std::endl;
 }
