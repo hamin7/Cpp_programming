3d0f15171a213ea01fedad5131d9efa6090123c8
hamin7 <33690039+hamin7@users.noreply.github.com>
2018-12-06 22:17:08 +0900

    Update 18_11_05(1).cpp

--- 18_11_05/18_11_05(1).cpp
+++ 18_11_05/18_11_05(1).cpp
@@ -1,11 +1,10 @@
 #include <iostream>
 
-// int couunter = 0;     // global variable
-int Circle::counter = 0;
+
 class Circle{
 public:
   static int counter;     // is merely a declaration
-  Circle(){ counter++; }      // default construct
+  Circle(){ counter++; }      // default construct.  기본생성자
 
   double radius;
 
@@ -13,8 +12,11 @@ public:
     return radius*radius*3.14;
   }
 };
+// int couunter = 0;     // global variable
+int Circle::counter = 0;        // Circle 클래스의 counter = 0으로 세팅
 
 void main(){
-  Circle c; Circle c1;
+  Circle c;          // 오브젝트 c 선언
+  Circle c1;         // 오브젝트 c1 선언
   std::cout << Circle::counter << std::endl;
 }
