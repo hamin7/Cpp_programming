dc4520bd389c894a98daa3f107776c6aa073fe91
hamin7 <33690039+hamin7@users.noreply.github.com>
2018-12-06 21:38:08 +0900

    Update 18_10_29(5).cpp

--- 18_10_29/18_10_29(5).cpp
+++ 18_10_29/18_10_29(5).cpp
@@ -3,17 +3,17 @@
 class Circle{
 public:
   // Constructor
-  Circle( double r ) : radius(r >= 0 ? r : 0){
+  Circle( double r ) : radius(r >= 0 ? r : 0){      // radius는 r>=0이라면 r을 리턴하고 아니라면 0을 리턴
   }
-  const double radius;     // attribute
-  double area()     // method
+  const double radius;     // attribute radius를 const로 정의
+  double area()     // method area()를 정의
   {
-    return radius * radius * 3.14;
+    return radius * radius * 3.14;     // 넓이는 radius * radius * 3.14
   };
 };
 
 void main()
 {
   Circle c(3.0);     // initialization of public members only!   // defines an object
-  std::cout << c.get_area() << std::endl;
+  std::cout << c.get_area() << std::endl;     // 오브젝트 c의 넓이를 구하여라
 }
