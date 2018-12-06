cd2e62ee20665de30a83cc8b4d6d93f187c5d075
hamin7 <33690039+hamin7@users.noreply.github.com>
2018-12-06 23:40:26 +0900

    Update 18_11_05(3).cpp

--- 18_11_05/18_11_05(3).cpp
+++ 18_11_05/18_11_05(3).cpp
@@ -1,21 +1,24 @@
 #include <iostream>
 
-class Circle{
+class Circle{           // Circle 클래스 정의
 private:
-  double radius;
+  double radius;        // 애트리뷰트 radius 선언
 public:
-  double area(){
-    return radius*radius*3.14;
+  double area(){         // 메소드 area() 선언 
+    return radius*radius*3.14;     // 넓이!!
   }
-  frined class ExtClass;
+  frined class ExtClass;         // Circle은 Extclass를 친구로 생각함!
 };
-class ExtClass{
-  Circle c;
-  void fun(){
-    c.radius = 2;
+class ExtClass{            // 클래스 ExtClass 선언 
+  Circle c;                // 오브젝트 c
+  void fun(){              // 메소드 fun()
+    c.radius = 2;           // 오브젝트 c의 radius에 2 대입
   }
 }
 void main(){
-  Circle c; Circle c1;
-  std::cout << std::endl;
+  Circle c; Circle c1;           // c에 
+  std::cout << c.area() << std::endl;
 }
+
+
+뭐야 이거 
