2e062995da2cafad2bebd816b1893c7600d3328c
hamin7 <33690039+hamin7@users.noreply.github.com>
2018-12-06 23:50:26 +0900

    Update 18_11_05(4).cpp

--- 18_11_05/18_11_05(4).cpp
+++ 18_11_05/18_11_05(4).cpp
@@ -1,16 +1,18 @@
 #include <iostream>
 class Circle{
 public:
-  Circle(double radius){ this->radius = radius; }
-  double radius;
-  double area(){
-    return radius*radius*3.14;
+  Circle(double radius){ this->radius = radius; }     // Circle 클래스 매개변수는 radius 이 클래스의 radius에 radius를 넣자
+  double radius;               // double 타입의 애트리뷰트 radius
+  double area(){               // double 타입의 매소드 area()
+    return radius*radius*3.14;       // 넓이를 구해서 리턴해라
   }
 };
-double operator+ (Circle &c1, Circle &c2){
-  return c1.radius + c2.raidus;
+double operator+ (Circle &c1, Circle &c2){         // 요건 double타입 매소드???
+  return c1.radius + c2.raidus;                    // c1과 c2의 반지름을 더해서 리턴해라
 }
 void main(){
-  Circle c(2); Circle c1(4);
-  std::cout << (c + c1) std::endl;
+  Circle c(2); Circle c1(4);            // c의 반지름은 2 c1의 반지름은 4
+  std::cout << (c + c1) << std::endl;           // 6 :: c와 c1을 더해라
 }
+
+이건 잘 돌아간다!
