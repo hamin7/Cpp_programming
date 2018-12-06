d6b5e86c2507e570e204b4cf58444bc7e5ecb3aa
hamin7 <33690039+hamin7@users.noreply.github.com>
2018-12-06 22:28:35 +0900

    Update 18_11_05(2).cpp

--- 18_11_05/18_11_05(2).cpp
+++ 18_11_05/18_11_05(2).cpp
@@ -7,12 +7,13 @@ public:
   double area(){
     return radius*radius*3.14;
   }
-  friend void fun( Circle &c );     // friend declaration
+  friend void fun( Circle &c );     // friend declaration  클래스 Circle은 메소드 fun을 친구로 생각
 };
-void fun( Circle &c ){      // &c 안하고 그냥 (Circle c) 하면 에러 남
-  c.radius = c.radius * 2;
+void fun( Circle &c ){      // &c 안하고 그냥 (Circle c) 하면 에러 남 / 왜???
+  c.radius = c.radius * 2;        // 
 }
 void main(){
-  Circle c; Circle c1;
+  Circle c; 
+  Circle c1;
   std::cout << Circle::counter << std::endl;
 }
