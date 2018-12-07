f85405b9555fa08d8fee1807925952424c144262
hamin7 <33690039+hamin7@users.noreply.github.com>
2018-12-07 20:08:58 +0900

    Update 18_11_05(5).cpp

--- 18_11_05/18_11_05(5).cpp
+++ 18_11_05/18_11_05(5).cpp
@@ -5,7 +5,7 @@ public:
   Circle(double radius){ this->radius = radius; }            // Circle 클래스의 생성자???  
   double radius;                                      // double 타입 애트리뷰트 radius
   double operator+ (Circle &c2){                     // double타입 매소드 operator 오버로딩
-    return radius;                      // return radius + c2.radous 하면 6 / return c2.radius 하면 4 / return radius 하면 2 출력
+    return radius + c2.radius;                      // return radius + c2.radous 하면 6 / return c2.radius 하면 4 / return radius 하면 2 출력
   }
 };
 
