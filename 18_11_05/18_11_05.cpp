7cba3387cc151a0195a62ff096c945fdf29eba0d
hamin7 <33690039+hamin7@users.noreply.github.com>
2018-12-07 17:15:59 +0900

    Update 18_11_05.cpp

--- 18_11_05/18_11_05.cpp
+++ 18_11_05/18_11_05.cpp
@@ -8,9 +8,9 @@ public:
     // this.radius = radius;   얘는 안됨 왜냐면 this는 클래스 자신을 가리키는 포인터이기 때문에 / 포인터 변수는 .으로 접근할 수 없다
   }
   void connect( Circle &c ){
-    c.link = this;         // 
+    c.link = this;         // c가 가지는 멤버 변수 포인터 link가 this(d객체의 주소값)를 가리키게 됨.
   }
-  Circle *link;
+  Circle *link;           // 그냥 포인터 정의만 된 
   double radius;
 
   double area(){
@@ -19,7 +19,10 @@ public:
 };
 
 void main(){
-
+  Circle c(1.0);
+	Circle d(2.0);
+	d.connect(c);
+	std::cout << c.link << std::endl;
 }
 
 역참조(dereference) https://dojang.io/mod/page/view.php?id=276
