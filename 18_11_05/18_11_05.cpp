9ee83062363c728eb19c3d05a74360afe90081f7
hamin7 <33690039+hamin7@users.noreply.github.com>
2018-12-07 17:11:32 +0900

    Update 18_11_05.cpp

--- 18_11_05/18_11_05.cpp
+++ 18_11_05/18_11_05.cpp
@@ -5,7 +5,7 @@ public:
   Circle( double radius ){          // double타입 radius를 매개변수로 갖는 Circle 생성자
     //(*this).radius = radius;      // dereferencing
     this->radius = radius;     //위에 꺼랑 같음
-    // this.radius = radius;
+    // this.radius = radius;   얘는 안됨 왜냐면 this는 클래스 자신을 가리키는 포인터이기 때문에 / 포인터 변수는 .으로 접근할 수 없다
   }
   void connect( Circle &c ){
     c.link = this;         // 
