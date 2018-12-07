843860c879d30065c6b02be01189728cb5e057fc
hamin7 <33690039+hamin7@users.noreply.github.com>
2018-12-07 20:55:23 +0900

    Update 18_11_12(1).cpp

--- 18_11_12/18_11_12(1).cpp
+++ 18_11_12/18_11_12(1).cpp
@@ -1,23 +1,26 @@
 #include <iostream>
 class Circle{
 protected:       // you can read and write the attribute we get access in subclasses
-  double radius;
+	double radius;
 public:
-  Circle() { this->radius = 0; }
-  Circle( double radius ) { this->radius = radius > 0 ? radius : 0; }  // { Circle::radius == this->radius }
-  double area() { return radius * radius * 3.14; }
+	Circle() { this->radius = 0; }
+	Circle( double radius ) { this->radius = radius > 0 ? radius : 0; }  // { Circle::radius == this->radius }
+	double area() { return radius * radius * 3.14; }
 };
-class Cylinder : public Cirle {
-  Cylinder( double raidus, double height ) : Circle(radius)     // // superclass constructor
-  {
-    // this->radius = radius > 0 ? radius : 0;     // code redundancy 아래 코드처럼 해야 함.
-    // Circle(radius);   // 이거 대신 앞에 Circle(radius)로 하기
-    this->height = height > 0 ? height : 0;
-  }
-  double height;
-  double volume() { return Circle::area() * height; }
-  double area() { 2 * radius * 3.14 * height + 2 * Circle::area(); }
+class Cylinder : public Circle {
+public:
+	Cylinder( double radius, double height ) : Circle(radius)     // // superclass constructor
+	{
+		// this->radius = radius > 0 ? radius : 0;     // code redundancy 아래 코드처럼 해야 함.
+		// Circle(radius);   // 이거 대신 앞에 Circle(radius)로 하기
+		this->height = height > 0 ? height : 0;
+	}
+	double height;
+	double volume() { return Circle::area() * height; }
+	double area() { 2 * radius * 3.14 * height + 2 * Circle::area(); }
 };
-void main(){
-
+int main(){
+	Cylinder c(3.0, 4.0);
+	std::cout << c.volume() << std::endl;
+	std::cout << c.area() << std::endl;
 }
