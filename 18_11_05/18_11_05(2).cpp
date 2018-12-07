653c0296a0df7738f992f42d787fbe6639ebacb0
hamin7 <33690039+hamin7@users.noreply.github.com>
2018-12-07 17:42:30 +0900

    Update 18_11_05(2).cpp

--- 18_11_05/18_11_05(2).cpp
+++ 18_11_05/18_11_05(2).cpp
@@ -2,20 +2,26 @@
 
 class Circle{
 private:
-  double radius;
+	
 public:
+	double radius;
   double area(){
     return radius*radius*3.14;
   }
   friend void fun( Circle &c );     // friend declaration  클래스 Circle은 메소드 fun을 친구로 생각
 };
-void fun( Circle &c ){      // &c 안하고 그냥 (Circle c) 하면 에러 남 / 왜???
+
+void fun( Circle &c ){      // &c 안하고 그냥 (Circle c) 하면 에러 남 / 왜??? 에러는 안나지만 결과적으로 c1의 radius값은 안바뀜 용량도 아낄수 있음 왜냐면 call by value는 복사해서 쓰므로 공간이 두배
   c.radius = c.radius * 2;        // 
 }
-void main(){
+
+int main(){
   Circle c; 
   Circle c1;
-  std::cout << Circle::counter << std::endl;
+	c.radius = 3.0;
+	c1.radius = 2.0;
+	std::cout << c1.radius << std::endl;
+  fun(c1);
+	std::cout << c1.radius << std::endl;
 }
 
-이거 뭔가 이상함
