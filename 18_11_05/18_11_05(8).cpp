46ba47e009dd9e55e3054140281a82ec6a16d1e4
hamin7 <33690039+hamin7@users.noreply.github.com>
2018-12-07 20:23:52 +0900

    Update 18_11_05(8).cpp

--- 18_11_05/18_11_05(8).cpp
+++ 18_11_05/18_11_05(8).cpp
@@ -1,15 +1,16 @@
 #include <iostream>
 class Circle{
 public:
-  double radius;
-  Circle(double radius){ this->radius = radius; }
+	double radius;
+	Circle(double radius){ this->radius = radius; }
 };
 std::ostream & operator<< ( std::ostream &out, Circle &c){     // only output stream
-  out << "Circle with radius " << c.radius;
-  return out;
+	out << "Circle with radius " << c.radius;
+	return out;
 }
 
-void main(){
-  Circle c(2); Circle c1(4);
-  (std::cout << c) << std::endl;
+int main(){
+	Circle c(2); 
+	Circle c1(4);
+	(std::cout << c1) << std::endl;      // 
 }
