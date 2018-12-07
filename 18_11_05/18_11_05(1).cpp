81131e398ea054deaebbe7fa69583b4ed55bca2c
hamin7 <33690039+hamin7@users.noreply.github.com>
2018-12-07 17:30:27 +0900

    Update 18_11_05(1).cpp

--- 18_11_05/18_11_05(1).cpp
+++ 18_11_05/18_11_05(1).cpp
@@ -9,14 +9,16 @@ public:
   double radius;
 
   double area(){
-    return radius*radius*3.14;
+     counter++;
+    //return radius*radius*3.14;
   }
 };
 // int couunter = 0;     // global variable
-int Circle::counter = 0;        // Circle 클래스의 counter = 0으로 세팅
+int Circle::counter = 0;        // Circle 클래스의 counter = 0으로 세팅.  얘 안에서 하면 안돌아감 왜냐면 그렇게 하면 static 하는 의미가 없으니까 객체 생성시마다 초기화가 되는거니까
 
 void main(){
   Circle c;          // 오브젝트 c 선언
   Circle c1;         // 오브젝트 c1 선언
+  c.area();
   std::cout << Circle::counter << std::endl;
 }
