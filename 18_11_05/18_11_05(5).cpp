a4c2e9601511b0faf857c18cf4919d64419a95f0
hamin7 <33690039+hamin7@users.noreply.github.com>
2018-12-07 19:18:56 +0900

    Update 18_11_05(5).cpp

--- 18_11_05/18_11_05(5).cpp
+++ 18_11_05/18_11_05(5).cpp
@@ -1,14 +1,16 @@
 #include <iostream>
 class Circle{
 public:
-  Circle(double radius){ this->radius = radius; }            // Circle 클래스의 생성자???
+  Circle(double radius){ this->radius = radius; }            // Circle 클래스의 생성자???  
   double radius;                                      // double 타입 애트리뷰트 radius
-  double operator+ (Circle &c2){                     // double타입 매소드 (Circle c2를 
+  double operator+ (Circle &c2){                     // double타입 매소드 operator 오버로딩
     return radius + c2.raidus;                      // radius 
   }
 };
 
-void main(){
-  Circle c(2); Circle c1(4);
-  std::cout << (c + c1) std::endl;
+int main(){
+  Circle c(2); 
+	Circle c1(4);
+	Circle c2(6);
+	std::cout << (c + c1) std::endl;
 }
