356d1caaa9b7f8c7d0d518d143f493c0c5f2bb21
hamin7 <33690039+hamin7@users.noreply.github.com>
2018-12-07 16:26:42 +0900

    Update 18_10_29(5).cpp

--- 18_10_29/18_10_29(5).cpp
+++ 18_10_29/18_10_29(5).cpp
@@ -15,5 +15,7 @@ public:
 void main()
 {
   Circle c(3.0);     // initialization of public members only!   // defines an object
+  // Circle c; 이렇게 하면 에러남 (컴파일러가 자동으로 기본생성자 안 만들어쥼 위에 기본생성자가 있었다면 괜찮)
+  // 클래스 정의할때 생성자 정의가 아무것도 없으면 기본생성자만 자동으로 생성 (7)참고
   std::cout << c.get_area() << std::endl;     // 오브젝트 c의 넓이를 구하여라
 }
