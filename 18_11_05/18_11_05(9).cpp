ffc80308b4662bb4332cb7a2dcd314e98d94f6a8
hamin7 <33690039+hamin7@users.noreply.github.com>
2018-12-07 20:44:06 +0900

    Update 18_11_05(9).cpp

--- 18_11_05/18_11_05(9).cpp
+++ 18_11_05/18_11_05(9).cpp
@@ -1,15 +1,25 @@
 #include <iostream>
 class Circle{
 public:
-  double radius;
-  double area() { return radius * radius * 3.14; }
-  Circle( double radius ) { this -> radius = radius; }
+	double radius;            // 반지름
+	double area() { return radius * radius * 3; }      // 넓이는 r*r*3.14
+	//Circle( double radius ) { this -> radius = radius; }      // Circle 클래스의 반지름은 매갸변수로 받은 radius
 };
-class Cylinder : public Circle {
+
+class Cylinder : public Circle {                 // Circle 클래스를 상속
 public:
-  double height;
-  double volume(){ return height * Circle::area(); }
-  // surface of the Cylinder
-  double area() { return 2 * Circle::area()
-    + 2 * radius * 3.14 * height; }
+	double height;               // 실린더의 높이
+	double volume(){ return height * Circle::area(); }     // 부피는 높이 * 넓이
+	// surface of the Cylinder
+	double area() { return 2 * Circle::area() + 2 * radius * 3 * height; }       //// 겉면적 == (윗면 넓이*2) + (2파이r * 높이)
 };
+
+int main(){
+	Cylinder y;
+	y.radius = 2.0;
+	y.height = 4.0;
+	std::cout << y.area() << std::endl;
+	std::cout << y.volume() << std::endl;
+}
+
+// line6이 있으면 안되는 이유는?
