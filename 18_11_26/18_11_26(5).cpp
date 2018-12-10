16a650634365c120faa71364f905aa47fdc569e3
hamin7 <33690039+hamin7@users.noreply.github.com>
2018-12-11 00:17:46 +0900

    Update 18_11_26(5).cpp

--- 18_11_26/18_11_26(5).cpp
+++ 18_11_26/18_11_26(5).cpp
@@ -7,15 +7,15 @@ TP max( TP a, TP b){
 return a > b ? a : b;
 }
 // template specialization
-template<>
-const char* max( const char *a, const char *b){
-return strcmp( a, b ) ? a : b;
-}
 
 int main(){
-// lexicographic grapic 이 큰게 출력 될것 여기서는 B
+// lexicographic grapic이 큰게 출력 될 것 여기서는 B
 const char * a = "A";
 const char * b = "B";
 const char * k = max( a, b );     // type inference
 std::cout << k << std::endl;
 }      // function
+
+/*
+b
+*/
